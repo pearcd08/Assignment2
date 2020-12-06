3ab32c15ab43202b6f7eac58932368a46b104d77
David Pearce <pearcd08@myunitec.ac.nz>
2020-12-05 21:36:29 -0800

    David

---
+++ Homepage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Petz Homepage</title>
+    <link rel = "stylesheet" type = "text/css" href = "css/stylesheet.css" >
+    <script src="js/script.js"></script>
+  </head>
+
+
+
+<body>
+
+<div id = "header">
+    <img src = "images/Header1.jpg.jpg">
+</div>
+
+    <div>
+        <img class="rotate_01" src="images/yarn.png">
+
+    </div>
+
+
+    <div id = "menu">
+        <ul>
+            <li><a href="Dog.php">Dog<</a></li>
+            <li><a href="Cat.php">Cat</a></li>
+            <li><a href="Bird.php">Bird</a></li>
+            <li><a href="Fish.php">Fish</a></li>
+        </ul>
+    </div>
+</body>
+<footer>
+    <p>
+        Petz Food Company, Auckland, New Zealand
+    </p></footer>
+</html>
